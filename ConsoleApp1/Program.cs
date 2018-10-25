8448755680356202042e1c4074177f7ac9e7d258
Max Groot <max.groot@outlook.com>
2018-10-25 12:41:15 +0200

    Rename namespace

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,24 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DecisionTrees
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program started");
-
             TrainingController trainer = new TrainingController();
             trainer.generateExamples();
 
             Algorithm algorithm = new ID3();
 
+            Console.WriteLine("Program started. Press a key to start loading training data. \n");
+            Console.ReadKey(true);
+
             // Load in everything we need for the training process
             List <DataInstance> exampleSet = trainer.exampleSet();
             List <string> attributeSet = trainer.exampleAttributes();
             string targetAttribute = trainer.targetAttribute();
-            Console.WriteLine("Training data loaded. ");
+
+            Console.WriteLine("Training data loaded. Press a key to start training process \n");
+            Console.ReadKey(true);
 
             // Train the algorithm based on the Training set
             Console.WriteLine("Starting Training process.");
