08aefbc810a19d970dbda83faa831b92109d9376
Max Groot <max.groot@outlook.com>
2018-11-04 20:14:08 +0100

    Renamings

---
+++ ConsoleApp1/Observations/ObservationSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTrees
+{
+    class ObservationSet: Premise
+    {
+        public List<DataInstance> instances;
+        public string target_attribute;
+        public List<string> attributes;
+
+        public ObservationSet (List<DataInstance> examples, string target_attribute, List<string> attributes)
+        {
+            this.instances = examples;
+            this.target_attribute = target_attribute;
+            this.attributes = attributes;
+        }
+    }
+}
