9f7d0a77689b7cdea79610f8e9ab592c0ce8b156
Max Groot <max.groot@outlook.com>
2018-11-05 09:38:44 +0100

    Add all outputs as thoughts, write thoughts to CSV

--- ConsoleApp1/Thinking/TextWriter.cs
+++ ConsoleApp1/Thinking/TextWriter.cs
@@ -12,35 +12,63 @@ namespace DecisionTrees
         private List<string> infer_lines = new List<string>();
         private List<string> decision_lines = new List<string>();
         private List<string> model_lines = new List<string>();
+        private List<Thought> thoughts = new List<Thought>();
 
         private string location;
 
         public TextWriter(string location)
         {
             this.location = location;
+
         }
         public void infer_add(string line)
         {
-            infer_lines.Add(line);
+            this.add_thought(ref infer_lines, "INFER", line);
         }
 
         public void decision_add(string line)
         {
-            decision_lines.Add(line);
+            this.add_thought(ref decision_lines, "DECIDE", line);
         }
 
         public void model_add(string line)
         {
-            model_lines.Add(line);
+            this.add_thought(ref model_lines, "PERFORM", line);
         }
-        
+
+        private void add_thought(ref List<string> list, string type, string line)
+        {
+            list.Add(line);
+
+            // Remove tabs and double spaces from thoughts. 
+            char tab = '\u0009';
+            line = line.Replace(tab.ToString(), "");
+            while (line.IndexOf("  ") > 0)
+            {
+                line = line.Replace("  ", " ");
+            }
+
+            this.thoughts.Add(new Thought(type, line));
+        }
         public void write()
         {
             System.IO.File.WriteAllLines(location + "infers.txt", infer_lines.ToArray());
             System.IO.File.WriteAllLines(location + "model.txt", model_lines.ToArray());
             System.IO.File.WriteAllLines(location + "decisions.txt", decision_lines.ToArray());
+            this.save_thoughts();
         }
 
+        public void save_thoughts()
+        {
+            string seperator = ",";
+            var csv = new StringBuilder();
+            csv.AppendLine($"Type{seperator}Value{seperator}");
+            foreach(Thought thought in thoughts)
+            {
+                csv.AppendLine($"{thought.type}{seperator}{thought.value}{seperator}");
+            }
+            File.WriteAllText(location + "thoughts.csv", csv.ToString());
+        }
         public static string askLocation(int output_line)
         {
             string ask = Console.ReadLine();
---
+++ ConsoleApp1/Thinking/Thought.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTrees
+{
+    class Thought
+    {
+        public string type;
+        public string value;
+
+        public Thought(string type, string value)
+        {
+            this.type = type;
+            this.value = value;
+        }
+    }
+}
