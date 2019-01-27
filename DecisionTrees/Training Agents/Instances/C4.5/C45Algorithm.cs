1048057ae2d3eec480369a4e26245f44b873e7ee
Max Groot <max.groot@outlook.com>
2019-01-27 17:00:57 +0100

    WIP C4.5 algorithm

--- DecisionTrees/Training Agents/Instances/C4.5/C45Algorithm.cs
+++ DecisionTrees/Training Agents/Instances/C4.5/C45Algorithm.cs
@@ -12,5 +12,44 @@ namespace DecisionTrees
         {
             throw new NotImplementedException();
         }
+
+        private DecisionTree iterate(DecisionTree tree, List<DataInstance> set, string target_attribute, List<string> attributes, Agent runner)
+        {
+            // Check termination criteria
+            if (termination_criteria_met(set, target_attribute))
+            {
+                return tree;
+            }
+            foreach(string attr in attributes)
+            {
+                double gainratio = Calculator.splitInfo()
+            }
+            // Compute criteria for attributes
+            double global_entropy = Calculator.entropy(set, target_attribute);
+
+            // Choose best attribute
+
+            // Create node for attribute
+
+            // Split the set based on newly created node
+
+            // For all sub data, call C4.5 to get a sub-tree
+
+            // Attach sub-tree to the node of 4.
+
+            // Return tree
+            return tree;
+        }
+
+
+        private bool termination_criteria_met(List<DataInstance> set, string target_attribute)
+        {
+            if (Calculator.subset_has_all_same_classifier(set, target_attribute))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
