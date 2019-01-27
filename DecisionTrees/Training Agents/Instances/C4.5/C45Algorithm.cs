14a31f9950b7e9de12b11bb27d417c1af2729828
Max Groot <max.groot@outlook.com>
2019-01-27 15:32:27 +0100

    Add c4.5 agent infrastructure

---
+++ DecisionTrees/Training Agents/Instances/C4.5/C45Algorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTrees
+{
+    class C45Algorithm : Algorithm
+    {
+        public DecisionTree train(List<DataInstance> examples, string target_attribute, List<string> attributes, Agent runner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
