bb52523c2266812410e38975e298ef0b366d1fe7
Max Groot <max.groot@outlook.com>
2019-05-06 13:49:03 +0200

    Attach data locations to tree, log actions

--- DecisionTrees/Training Agents/Instances/ID3/ID3Algorithm.cs
+++ DecisionTrees/Training Agents/Instances/ID3/ID3Algorithm.cs
@@ -54,21 +54,31 @@ namespace DecisionTrees
 
         public DecisionTree iterate(DecisionTree tree, List<DataInstance> sets_todo, int level, List<string> considerable_attributes, Node parent_node, string parent_value_splitter)
         {
+            Console.WriteLine("Iterate");
+
             List <string> attributes_copy = new List<string>(considerable_attributes.ToArray());
             // Find best possible way to split these sets. For each attribute we will calculate the gain, and select the highest.
             string best_attr = "UNDETERMINED";
             double highest_gain = 0;
             foreach(string attr in attributes_copy)
-            { 
+            {
+                Console.WriteLine("Consider attribute");
                 double my_gain = Calculator.gain(sets_todo, attr, this.target_attribute, this.possible_attribute_values[attr]);
+                Console.WriteLine($"Calculate Attribute Gain ({attr} = {my_gain})");
                 runner.THINK(calculate_attribute_gain, my_gain, attr);
                 if (my_gain > highest_gain)
                 {
                     best_attr = attr;
                     highest_gain = my_gain;
                     runner.THINK(determine_best_attribute, highest_gain, best_attr);
+                    Console.WriteLine("Set new best attribute");
+                }
+                else
+                {
+                    Console.WriteLine("Do not change best attribute");
                 }
             }
+            Console.WriteLine("End loop");
             if (highest_gain == 0)
             {
                 // This set cannot be split further.
@@ -76,24 +86,29 @@ namespace DecisionTrees
                 // This happens when instances have all attributes the same except for the classifier.
                 string classifier_value = SetHelper.mostCommonClassifier(sets_todo, target_attribute);
                 tree.addBestGuessLeaf(parent_value_splitter, classifier_value, parent_node);
+                Console.WriteLine("Add best guess leaf");
                 return tree;
             }
 
             // The best attribute to split this set is now saved in best_attr. Create a node for that.
             // Remove this attribute as a splitter for the dataset.
             attributes_copy.RemoveAt(considerable_attributes.IndexOf(best_attr));
+            Console.WriteLine("Remove attribute from consideration");
             
             // Parent value splitter is to give a node an idea what it's parent splitted on. For decision rules this is needed information.
             Node new_node = tree.addNode(best_attr, parent_value_splitter, parent_node);
-            
+            Console.WriteLine("Add Node");
             // Create subsets for each possible value of the attribute we created a node for. 
+            Console.WriteLine("Loop on possible values");
             foreach (string value_splitter in this.possible_attribute_values[best_attr])
             {
+                Console.WriteLine($"Create subset with this value. ({value_splitter})");
                 List<DataInstance> subset = sets_todo.Where(A => A.getProperty(best_attr) == value_splitter).ToList();
                 runner.THINK(split_on_best_attribute, subset_to_objectstring(subset), list_to_objectstring(attributes_copy), new_node.identifier, value_splitter);
                 if (subset.Count == 0)
                 {
                     // There are no more of this subset. We need to skip this iteration.
+                    Console.WriteLine("DO NOTHING");
                     continue;
                 }
                 if (SetHelper.hasUniformClassifier(subset, target_attribute))
@@ -102,15 +117,20 @@ namespace DecisionTrees
                     // Each leaf represents one decision rule. 
                     string classifier_value = subset.First().getProperty(target_attribute);
                     Leaf leaf = tree.addLeaf(value_splitter, classifier_value, new_node);
+                    tree.data_locations[leaf] = subset;
+                    Console.WriteLine("ADD LEAF");
                 } else
                 {
                     // We still haven't resolved this set. We need to iterate upon it to split it again. 
-
+                    Console.WriteLine("Iterate deeper");
                     this.iterate(tree, subset, level+1, attributes_copy, new_node, value_splitter);
 
                     // If we got here in the code then the set that was previously not all the same classifier has been resolved. We need to move up.
                 }
+                Console.WriteLine("Continue to next attribute");
+                
             }
+            Console.WriteLine("RETURN TREE TO SELF");
             // We have succesfully split all examples on this attribute. Return the tree in its current state. 
             return tree;
         }
@@ -138,6 +158,7 @@ namespace DecisionTrees
                 this.possible_attribute_values.Add(attr, attribute_values);
             }
         }
+
         private string subset_to_objectstring(List<DataInstance> l)
         {
             string ret = "{";
